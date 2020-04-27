1f0a592e4e4b7f00a8abb03ddc04e2bba625ae63
Serena Do <doserena98@gmail.com>
2020-04-26 23:35:15 -0400

    Implemented now playing methods for MusicTabView

--- RunningSkeleton/MoonRunner/View Controllers/MusicTabViewController.swift
+++ RunningSkeleton/MoonRunner/View Controllers/MusicTabViewController.swift
@@ -31,38 +31,125 @@
 import UIKit
 
 class MusicTabViewController: UIViewController {
+
+    @IBOutlet var trackName: UILabel!
+    @IBOutlet var artistName: UILabel!
+    @IBOutlet var albumArtImageView: UIImageView!
+    
+    private var subscribedToPlayerState: Bool = false
+    private var playerState: SPTAppRemotePlayerState?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if appRemote?.isConnected == true {
+      appRemoteConnected()
+    }
+  }
   
+  var defaultCallback: SPTAppRemoteCallback {
+      get {
+          return {[weak self] _, error in
+              if let error = error {
+                 print(error as NSError)
+              }
+          }
+      }
+  }
+
+  var appRemote: SPTAppRemote? {
+      get {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.appRemote
+      }
+  }
+  
+  // update view with now playing
+  private func updateNowPlaying(_ playerState: SPTAppRemotePlayerState) {
+    trackName.text = playerState.track.name
+    artistName.text = playerState.track.artist.name
+    fetchAlbumArtForTrack(playerState.track) { (image) -> Void in
+        self.updateAlbumArtWithImage(image)
+    }
+  }
+  
+  // get what is currently playing
+  private func getPlayerState() {
+      appRemote?.playerAPI?.getPlayerState { (result, error) -> Void in
+          guard error == nil else { return }
+          let playerState = result as! SPTAppRemotePlayerState
+          self.updateNowPlaying(playerState)
+      }
+  }
+  
+  // set up subscriber for what is playing
+  private func subscribeToPlayerState() {
+      guard (!subscribedToPlayerState) else { return }
+      appRemote?.playerAPI!.delegate = self
+      appRemote?.playerAPI?.subscribe { (_, error) -> Void in
+          guard error == nil else { return }
+          self.subscribedToPlayerState = true
+      }
+  }
   
   //Play Pause Button
   @IBAction func Button(_ sender: UIButton) {
-    if sender.currentTitle == "pause"{
-      //play music here!
-      sender.setTitle("play", for: UIControl.State.normal);
+    if sender.currentTitle == "play" {
+      appRemote?.playerAPI?.resume(defaultCallback)
+      sender.setTitle("pause", for: UIControl.State.normal)
     }
-    else if sender.currentTitle == "play"{
+    else if sender.currentTitle == "pause"{
       //pause music here!
-      sender.setTitle("pause", for: UIControl.State.normal);
+      appRemote?.playerAPI?.pause(defaultCallback)
+      sender.setTitle("play", for: UIControl.State.normal)
     }
   }
   
-  
+  // initial connection
+  func appRemoteConnected() {
+    subscribeToPlayerState()
+    getPlayerState()
+  }
+  // next song
   @IBAction func NextSong(_ sender: Any) {
-    
+    appRemote?.playerAPI?.skip(toNext: defaultCallback)
     print("Next Song Pressed!");
-    
   }
   
+  // previous song
   @IBAction func PreviousSong(_ sender: Any) {
-    
+    appRemote?.playerAPI?.skip(toPrevious: defaultCallback)
     print("Previous Song Pressed!");
-    
   }
   
+  // TODO: to implement
   @IBAction func SetBPM(_ sender: Any) {
-  
     print("Set BPM Pressed!");
   
   }
   
+  // slowly transition to new artwork
+  private func updateAlbumArtWithImage(_ image: UIImage) {
+      self.albumArtImageView.image = image
+      let transition = CATransition()
+      transition.duration = 0.3
+      transition.type = CATransitionType.fade
+      self.albumArtImageView.layer.add(transition, forKey: "transition")
+  }
   
+  // get album art for new track
+  private func fetchAlbumArtForTrack(_ track: SPTAppRemoteTrack, callback: @escaping (UIImage) -> Void ) {
+      appRemote?.imageAPI?.fetchImage(forItem: track, with:CGSize(width: 300, height: 300), callback: { (image, error) -> Void in
+          guard error == nil else { return }
+
+          let image = image as! UIImage
+          callback(image)
+      })
+  }
+}
+ 
+extension MusicTabViewController: SPTAppRemotePlayerStateDelegate {
+       func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+           self.playerState = playerState
+           updateNowPlaying(playerState)
+       }
 }
---
+++ RunningSkeleton/TempoRun-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  TempoRun-Bridging-Header.h
+//  TempoRun
+//
+//  Created by Serena Do on 4/20/20.
+//  Copyright © 2020 Yodahe Alemu. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
